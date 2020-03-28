b55b617b36050189de0b3c98c19c098c37c1603f
Stephanie Raymos <stephanieraymos@gmail.com>
2020-03-28 12:53:36 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//carousel pause and play
 $(function() {
   $(".carousel").carousel( { interval: 2000 } );
   $("#carouselButton").click(function(){
@@ -11,4 +12,27 @@ $(function() {
           $("#carouselButton").children("i").addClass("fa-pause"); 
       }
   });
-});
+
+//Login button modal show
+   $("#loginButton").click(function(){
+    $("#loginModal").modal("show");
+    // if ($(".close").modal("hide"));
+ //Login button modal hide
+  });
+  $(".close").click(function(){
+    $("#loginModal").modal("hide");
+
+  });
+//Reserve button modal show
+    $("#reserveButton").click(function(){
+    $("#reserveModal").modal("show")
+    });
+//Login button modal hide
+    $(".close").click(function(){
+        $("#reserveModal").modal("hide");
+    
+      });
+  });
+  //gallery image clicks
+  $("img").click(function(){
+    $("img").modal("show");
